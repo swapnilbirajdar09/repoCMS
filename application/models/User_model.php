34148aa68138e5943dc057ce31d03822f4e88f82
samratBizmo007 <samratbizmotech@gmail.com>
2019-04-03 15:30:37 +0530

    commit: email exist check

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -34,6 +34,17 @@ class User_model extends CI_Model {
         extract($data);
         $entityJson='';
 
+        // check email exist or not
+        $checkEmailExist=$this->checkEmailExist($user_email);
+        if($checkEmailExist){
+            $response=array(
+                'status'    =>  false,
+                'type'  =>  'warning',
+                'message'   =>  'Email Id already exist!'
+            );
+            return $response;
+            die();
+        }
         // check key exist and if it is array
         if(array_key_exists('entity_id', $data)){
             if(is_array($entity_id)){
@@ -55,20 +66,88 @@ class User_model extends CI_Model {
         );
         $this->db->insert('user_tab', $insert_data);
         if ($this->db->affected_rows() > 0){
-           $response = array(
+         $response = array(
             'status' => true,
             'type' => 'success',
             'message' => 'Subadmin created successfully'
         );
-           return $response; 
-       }
-       else{
+         return $response; 
+     }
+     else{
         $response = array(
             'status' => false,
             'type' => 'error',
             'message' => 'Subadmin user not created!'
         );
         return $response;
+    }
+}
+
+    // create user/admin function
+public function createAdmin($data) {
+    extract($data);
+    $entityJson='';
+
+    // check email exist or not
+    $checkEmailExist=$this->checkEmailExist($user_email);
+    if($checkEmailExist){
+        $response=array(
+            'status'    =>  false,
+            'type'  =>  'warning',
+            'message'   =>  'Email Id already exist!'
+        );
+        return $response;
+        die();
+    }
+
+        // check key exist and if it is array
+    if(array_key_exists('entity_id', $data)){
+        if(is_array($entity_id)){
+            $entityJson=json_encode($entity_id);
+        }
+        else{
+            $entityJson=$entity_id;
+        }
+    }
+        // insert new data
+    $insert_data = array(
+        'role_id'   => '2',
+        'user_email' => $user_email,
+        'password' => $user_password,
+        'app_id' => $app_id,
+        'entity_id' => $entityJson,
+        'added_date' => date('Y-m-d H:i:s'),
+        'status' =>  1
+    );
+    $this->db->insert('user_tab', $insert_data);
+    if ($this->db->affected_rows() > 0){
+     $response = array(
+        'status' => true,
+        'type' => 'success',
+        'message' => 'Admin created successfully'
+    );
+     return $response; 
+ }
+ else{
+    $response = array(
+        'status' => false,
+        'type' => 'error',
+        'message' => 'Admin user not created!'
+    );
+    return $response;
+}
+}
+
+// check email id exist in user table or not
+public function checkEmailExist($email){
+    $checkEmail=$this->db->get_where('user_tab', array('user_email =' => $email))->result();
+    if(empty($checkEmail)){
+        // email not exist
+        return false;
+    }
+    else{
+        // email exists
+        return true;
     }
 }
 
