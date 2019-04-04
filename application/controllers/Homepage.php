b604d9cb8e5102e3fed653d3490a73c96f1c0e4a
samratBizmo007 <samratbizmotech@gmail.com>
2019-04-04 18:13:18 +0530

    Create Homepage.php

---
+++ application/controllers/Homepage.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Homepage extends CI_Controller {
+
+    // Login controller
+    public function __construct() {
+        parent::__construct();
+    }
+
+    // main index function
+    public function index() { 
+
+    }
+    
+    // add new section
+    
+
+}
